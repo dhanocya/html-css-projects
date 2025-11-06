09a6928cbc0ecf24dec62843ca3de14db8d7516a
dhanraj singh <dhanocya@gamil.com>
2025-11-06 08:42:58 +0530

    some new code

--- project6/script.js
+++ project6/script.js
@@ -1,6 +1,14 @@
-const hambar = document.querySelector("#hambar")
-const box = document.querySelector(".box")
+const hambar = document.querySelector("#hambar");
+const box = document.querySelector(".box");
 
-hambar.addEventListener("click", () =>{
-    box.classList.toggle("seeBox")
-})
+hambar.addEventListener("click", () => {
+    box.classList.toggle("seeBox");
+    
+    if (hambar.classList.contains("fa-bars")) {
+        hambar.classList.remove("fa-bars")
+        hambar.classList.add("fa-xmark")
+    }else {
+        hambar.classList.remove("fa-xmark")
+        hambar.classList.add("fa-bars")
+    }
+});
